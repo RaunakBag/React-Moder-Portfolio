b94e70ada4545d90114265ac419f6946d1941235
Raunak Bag <raunakbag97@gmail.com>
2023-01-06 20:09:47 +0530

    CV Updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={'https://drive.google.com/file/d/1-bjr3vwWH0UgJpoLblyk6XgBvzR0l5Wi/view?usp=sharing'} download className="btn">
+      <a href={'https://drive.google.com/file/d/1sHlZfRqhg-yXnDvOOG9OJjVGceZeADAz/view?usp=sharing'} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
