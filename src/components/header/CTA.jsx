c9451d30fb5db67482ac17efc469aeb99d9fb082
Raunak Bag <79838018+RaunakBag@users.noreply.github.com>
2023-12-25 19:15:37 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={'https://drive.google.com/file/d/1mYR8_NP1jJW7XV1zrRuY4xz0229P95Hb/view?usp=sharing'} download className="btn">
+      <a href={'https://drive.google.com/file/d/1uF_DD-sJTt0Afd-tZMqT6bUuKvuqeEu7/view'} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
